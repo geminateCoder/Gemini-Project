0404cbd42f5d07e90a3b2847bb70deda4ae04827
GizMax <geminateCoder@live.com>
2016-07-09 07:29:25 -0400

    Added models to Admin

--- rp_grounds/home/admin.py
+++ rp_grounds/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import NewsPost, Goal, Feature, Staff
+
+admin.site.register(NewsPost)
+admin.site.register(Goal)
+admin.site.register(Feature)
+admin.site.register(Staff)
